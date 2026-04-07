1871829acaa13630ba3e7759e03421619ead8d35
kasperianunti-gif <kape.anunti@gmail.com>
2026-04-07 15:22:30 +0300

    Delete CategoryFilter.tsx

--- CategoryFilter.tsx
+++
@@ -1,9 +0,0 @@
-
-export async function fetchProductsFromApi() {
-   const response = await fetch('https://dummyjson.com/products');
-   if (!response.ok) {
-      throw new Error('Verkkovirhe tuotteita haettaessa');
-   }
-   const data = await response.json();
-   return data.products;
-};
